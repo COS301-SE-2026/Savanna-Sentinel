ac50b583b24b107526afed2003792962d6e087cd
stephan-kritzinger <u24581705@tuks.co.za>
2026-05-14 08:57:14 +0200

    Added decode method, implementing a verify method

--- backend/app/services/jwt_service.py
+++ backend/app/services/jwt_service.py
@@ -8,3 +8,10 @@ SECRET_KEY = os.getenv("JWT_SECRET")
 
 def encode(body):
     return jwt.encode(body, SECRET_KEY, algorithm="HS256")
+
+#Returns the decoded body
+def decode(token):
+    return jwt.decode(token, SECRET_KEY, algorithm="HS256")
+
+def verify(token):
+    body = decode(token)
