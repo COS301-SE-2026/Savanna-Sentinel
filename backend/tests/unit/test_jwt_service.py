4d72f674ee75ee53185c10a8600744d28be758f3
stephan-kritzinger <u24581705@tuks.co.za>
2026-05-14 10:34:50 +0200

    Created more test cases

--- backend/tests/unit/test_jwt_service.py
+++ backend/tests/unit/test_jwt_service.py
@@ -21,3 +21,21 @@ def test_enocde_and_decode_success(valid_user_data):
     assert decoded_payload["username"] == valid_user_data.get("username")
     assert "exp" in decoded_payload
 
+def test_encode_invalid_data_raises_validation_error():
+    """Test that passing invalid data types raises a ValueError"""
+
+    bad_data = {
+        "id": "not-an-int",
+        "username": "Bam",
+        "email": "bad-email",
+        "is_active": True
+    }
+
+    with pytest.raises(ValueError):
+        jwt_service.encode(bad_data)
+
+def test_decode_invalid_token():
+    """Test that a gardbage token returns None"""
+
+    result = jwt_service.decode("this.is.not.a.real.jwt.token")
+    assert result is None
