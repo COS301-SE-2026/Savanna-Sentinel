74e86ddb6c25294b0eaf80e686deabb6a32d137c
stephan-kritzinger <u24581705@tuks.co.za>
2026-05-14 10:13:09 +0200

    Added verify method

--- backend/app/services/jwt_service.py
+++ backend/app/services/jwt_service.py
@@ -37,4 +37,11 @@ def decode(token):
         return None
 
 def verify(token):
-    body = decode(token)
+    payload = decode(token)
+    if not payload:
+        return None
+    try:
+        return Token_Body(**payload)
+    except ValidationError as e:
+        print("Token payload structure is corrupt: {e}")
+        return None
