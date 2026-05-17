102573a10b95ed88e2b7a99e5352730887600b0c
stephan-kritzinger <u24581705@tuks.co.za>
2026-05-17 13:11:54 +0200

    Forgot to save the file

--- backend/app/schemas/user.py
+++ backend/app/schemas/user.py
@@ -1,1 +1,19 @@
 from pydantic import BaseModel
+from datetime import datetime
+
+class UserResponse(BaseModel):
+    id: str
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    role: str
+    is_active: bool
+    created_at: datetime
+
+
+class UserResultResponse(BaseModel):
+    total: int
+    page: int
+    page_size: int
+    results: UserResponse
