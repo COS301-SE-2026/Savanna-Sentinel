82fcf9d6c55f73bf02fd5637606fd10007809af2
stephan-kritzinger <u24581705@tuks.co.za>
2026-05-17 16:28:15 +0200

    Adding a feature that checks if the JWT claims the user is an admin

--- backend/app/core/dependencies.py
+++ backend/app/core/dependencies.py
@@ -1,10 +1,21 @@
 from typing import AsyncGenerator
-
+from fastapi import Depends, HTTPException, status
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from sqlalchemy.ext.asyncio import AsyncSession
+from app.models.user import User
+from app.services.jwt_service import verify
 
 from app.core.database import AsyncSessionLocal
 
+security_scheme = HTTPBearer()
 
 async def get_db() -> AsyncGenerator[AsyncSession, None]:
     async with AsyncSessionLocal() as session:
         yield session
+
+async def get_current_user(
+        credentials: HTTPAuthorizationCredentials = Depends(security_scheme),
+        db: AsyncSession = Depends(get_db)
+) -> User:
+    token = credentials.credentials
+    
