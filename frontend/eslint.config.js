9a52049faa9cb78f4317d25c248cf12b17327203
stephan-kritzinger <u24581705@tuks.co.za>
2026-06-29 11:21:39 +0200

    Huge insertions due to package-lock, cant do anything about it

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -4,6 +4,7 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import stylistic from '@stylistic/eslint-plugin'
+import reactPlugin from 'eslint-plugin-react'
 import { defineConfig, globalIgnores } from 'eslint/config'
 
 export default defineConfig([
@@ -14,7 +15,8 @@ export default defineConfig([
   {
     files: ['**/*.{ts,tsx}'],
     plugins: {
-      "@stylistic": stylistic
+      "@stylistic": stylistic,
+      "react": reactPlugin
     },
     extends: [
       js.configs.recommended,
@@ -24,6 +26,19 @@ export default defineConfig([
     ],
     languageOptions: {
       globals: globals.browser,
+      parser: tseslint.parser,
+      parserOptions: {
+        project: ["./tsconfig.app.json"],
+        tsconfigRootDir: import.meta.dirname,
+        ecmaFeatures: {
+          jsx: true,
+        },
+      }
+    },
+    settings: {
+      react: {
+        version: "19.2"
+      }
     },
     rules: {
       //Enforce strict equality
@@ -35,7 +50,7 @@ export default defineConfig([
       //typescript rules
       //If overloads are not consecutive, throw an error
       "@typescript-eslint/adjacent-overload-signatures": "error",
-      "@typescript-eslint/array-type": ["error", "array"],
+      "@typescript-eslint/array-type": ["error", { "default": "array" }],
 
       //Enforce variable names according to standard
       "@typescript-eslint/naming-convention": [
