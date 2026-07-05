d51e9d941e0521d9d4f03e58ae76387017499a83
stephan-kritzinger <u24581705@tuks.co.za>
2026-07-05 10:34:49 +0200

    Start of validating file schema

--- frontend/src/pages/IngestionPage.tsx
+++ frontend/src/pages/IngestionPage.tsx
@@ -1,9 +1,41 @@
 import React, { useState } from "react";
 import { type ChangeEvent } from "react";
 
+const FILE_SCHEMA = [
+    "record_id",
+    "ingestion_timestamp",
+    "source_system",
+    "data_domain",
+    "event_type",
+    "payload_size_kb",
+    "priority_level",
+    "retry_count",
+    "is_encrypted",
+    "status",
+];
+
 const IngestionPage = () => {
     const [selectedFile, setSelectedFile] = useState<File | null>(null);
     const [errorMessage, setErrorMessage] = useState<string | null>(null);
+
+    const validateSchema = (file: File) => {
+        const reader = new FileReader();
+
+        reader.onload = (e) => {
+            const text = e.target?.result;
+            if (typeof text === "string") {
+                //Temporary since want to parse things
+                return;
+            }
+        };
+
+        reader.onerror = () => {
+            setErrorMessage("Error reading the file. Please contact support.");
+            setSelectedFile(null);
+        };
+
+        reader.readAsText(file);
+    };
 
     const handleFileUpload = (event: ChangeEvent<HTMLInputElement>) => {
         const files = event.target.files;
@@ -29,18 +61,13 @@ const IngestionPage = () => {
         //Clear error messages
         setErrorMessage(null);
         setSelectedFile(file);
-        //Temporary log for debugging and testing purposes
-        console.log(file);
+        validateSchema(file);
     };
     return (
         <div>
             <h1>Example File Upload location</h1>
             {/* File type should always be .csv but adding it for dynamic reasons*/}
             <input type="file" accept=".csv" onChange={handleFileUpload} />
-            <p>
-                File Name:{" "}
-                {selectedFile ? selectedFile.name : "No file selected"}
-            </p>
             {errorMessage && <p>{errorMessage}</p>}
         </div>
     );
