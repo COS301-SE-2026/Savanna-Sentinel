5817cd71db46f702ea12002c352222d76d852f0c
stephan-kritzinger <u24581705@tuks.co.za>
2026-07-06 12:04:34 +0200

    Added file error and malformed header error test cases

--- frontend/src/tests/IngestionPage.test.tsx
+++ frontend/src/tests/IngestionPage.test.tsx
@@ -44,4 +44,68 @@ describe("Rendering tests - File upload errors (not content) ", () => {
         expect(error).toBeInTheDocument();
         expect(error).toHaveStyle({ color: "rgb(255, 0, 0)" });
     });
+
+    it("Invalid file format, first row is malformed, should display malformed error", async () => {
+        const user = userEvent.setup();
+        renderIngestionPage();
+
+        const csvFile = new File(["id,wrong_col\n"], "invalid_headers.csv", {
+            type: "text/csv",
+        });
+        const fileInput = screen.getByLabelText("CSV file upload");
+
+        if (!fileInput) {
+            throw new Error(
+                "Could not find the file input element - Invalid file format, first row is malformed...'",
+            );
+        }
+
+        await user.upload(fileInput, csvFile);
+        const expectedError =
+            "Invalid first row, please ensure that the first row matches the expected schema.";
+        const error = await screen.findByText(expectedError);
+
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveStyle({ color: "rgb(255, 0, 0)" });
+    });
+
+    it("Valid file format, browser experiences error reading file", async () => {
+        const user = userEvent.setup();
+        renderIngestionPage();
+
+        //Mock file reader to make it throw an error everytime, simulating file error
+        const readAsTextSpy = vi
+            .spyOn(FileReader.prototype, "readAsText")
+            .mockImplementation(function (this: FileReader) {
+                const error = new ProgressEvent("error", {
+                    lengthComputable: false,
+                    loaded: 0,
+                    total: 0,
+                });
+
+                if (this.onerror) {
+                    this.onerror(error as ProgressEvent<FileReader>);
+                }
+            });
+
+        const csvFile = new File(["id,status\n"], "test.csv", {
+            type: "text/csv",
+        });
+        const fileInput = screen.getByLabelText("CSV file upload");
+
+        if (!fileInput) {
+            throw new Error(
+                "Could not find the file input element - Valid file format, browser experiences error reading file'",
+            );
+        }
+
+        await user.upload(fileInput, csvFile);
+        const expectedError = "Error reading the file. Please contact support.";
+        const error = await screen.findByText(expectedError);
+
+        expect(error).toBeInTheDocument();
+        expect(error).toHaveStyle({ color: "rgb(255, 0, 0)" });
+
+        readAsTextSpy.mockRestore();
+    });
 });
