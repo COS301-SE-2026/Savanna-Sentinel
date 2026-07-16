34362beae825ec84d690871c550f53295ae5bac1
davidvrjn <u24569039@tuks.co.za>
2026-07-16 03:27:40 +0200

    added user search/filter + table style constants

--- frontend/src/lib/utils.ts
+++ frontend/src/lib/utils.ts
@@ -4,3 +4,7 @@ import { twMerge } from "tailwind-merge";
 export function cn(...inputs: ClassValue[]) {
     return twMerge(clsx(inputs));
 }
+
+export function formatRole(role: string) {
+    return role.replace(/_/g, " ").replace(/\b\w/g, (c) => c.toUpperCase());
+}
