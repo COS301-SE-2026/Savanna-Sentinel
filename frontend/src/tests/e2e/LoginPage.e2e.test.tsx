76b6e0e5544f27da3739d98c6bb780716888670a
Stephan-Kritzinger <u24581705@tuks.co.za>
2026-07-19 09:42:27 +0200

    Switched over from selenium to playwright

--- frontend/src/tests/e2e/LoginPage.e2e.test.tsx
+++ frontend/src/tests/e2e/LoginPage.e2e.test.tsx
@@ -1,33 +1,9 @@
-import {Browser, Builder, type ThenableWebDriver } from "selenium-webdriver"
-import { describe, it, expect, beforeAll, afterAll} from "vitest"
-import chrome from "selenium-webdriver/chrome"
+import {test, expect} from "@playwright/test"
 
-const BASE_URL = " http://localhost:5173"
+test("Example 1", async ({ page }) => {
+    await page.goto("/login");
+    const title = await page.title();
+    console.log("Logged Title: ", title);
 
-describe("Example test", () => {
-    let driver: ThenableWebDriver
-    beforeAll(async () => {
-        const options = new chrome.Options()
-        options.addArguments("--headless=new")
-        options.addArguments("--no-sandbox")
-        options.addArguments("--disable-dev-shm-usage")
-
-        driver = new Builder()
-        .forBrowser(Browser.CHROME)
-        .setChromeOptions(options)
-        .build()
-    })
-
-    afterAll(async () => {
-        if(driver){
-            await driver.quit()
-        }
-    })
-
-    it("Example 1", async () => {
-        await driver.get(`${BASE_URL}/login`);
-        const title = driver.getTitle();
-        console.log("Logged Title:", title)
-        expect(title).toBeDefined()
-    })
+    await expect(page).toHaveTitle(/Savanna Sentinel/)
 })
