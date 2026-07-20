28c21eb17d3539b36c2404e0c3ccbf1b8c826149
Sashumi-6 <danielcohen448@gmail.com>
2026-07-20 10:53:20 +0200

    initial setup

---
+++ frontend/src/pages/HelpPage.tsx
@@ -0,0 +1,16 @@
+// Quick access to VALUABLE RESOURCES such as help center links, tutorials and FAQs
+
+// CHECKLIST
+// [] FAQ (first or nah?)
+// [] Dashboard
+// [] Map
+// [] Reports
+// [] Patrol Planner
+// [] Profile
+// [] Link to user manual
+
+export default function HelpPage() {
+    return (
+        <></>
+    )
+}
