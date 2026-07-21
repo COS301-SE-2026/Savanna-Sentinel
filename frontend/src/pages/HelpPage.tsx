f63ad56ad9df17284c7d3ad4c695e070bc11ff83
Sashumi-6 <danielcohen448@gmail.com>
2026-07-21 14:04:40 +0200

    formatting

--- frontend/src/pages/HelpPage.tsx
+++ frontend/src/pages/HelpPage.tsx
@@ -19,7 +19,12 @@ import {
 
 function Faq() {
     return (
-        <></>
+        <Card>
+            <CardHeader>
+                <CardTitle className="text-lg">Placeholder</CardTitle>
+                <CardDescription className="text-base">Placerholder</CardDescription>
+            </CardHeader>
+        </Card>
     )
 }
 
@@ -27,7 +32,7 @@ function Dashboard() {
     return (
         <Card>
             <CardHeader>
-                <CardTitle>Placeholder</CardTitle>
+                <CardTitle className="text-lg">Placeholder</CardTitle>
             </CardHeader>
         </Card>
     )
@@ -38,8 +43,8 @@ function Map() {
     return(
         <Card>
             <CardHeader>
-                <CardTitle>Time Range Slider</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Time Range Slider</CardTitle>
+                <CardDescription className="text-base">
                     <ul>
                         <li>You may drag the slider to increase or decrease the view date</li>
                         <li>The time range slider allows one to see the risk map at past time intervals</li>
@@ -47,8 +52,8 @@ function Map() {
                 </CardDescription>
 
 
-                <CardTitle>Layers</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Layers</CardTitle>
+                <CardDescription className="text-base">
                     <ul>
                         <li>You can select and deselect multiple options</li>
                         <li>Risk Heatmap: toggles the risk heatmap overlay</li>
@@ -60,8 +65,8 @@ function Map() {
                 </CardDescription>
 
 
-                <CardTitle>Summary</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Summary</CardTitle>
+                <CardDescription className="text-base">
                     <ul>
                         <li>Critical cells: Indicates the number of cells marked as high-risk and high-alert</li>
                         <li>High-risk cells: Indicates the number of cells marked as being high-risk areas</li>
@@ -71,16 +76,14 @@ function Map() {
                 </CardDescription>
 
 
-                <CardTitle>Risk Level</CardTitle>
-                <CardDescription>
-                    <ul>
-                        <li>Indicates the colour mapping legend for different risk zone cells</li>
-                    </ul>
+                <CardTitle className="text-lg">Risk Level</CardTitle>
+                <CardDescription className="text-base">
+                    <p>Indicates the colour mapping legend for different risk zone cells</p>
                 </CardDescription>
 
 
-                <CardTitle>Model</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Model</CardTitle>
+                <CardDescription className="text-base">
                     <ul>
                         <li>Explains AI model was used to produce the heatmap</li>
                         <li>F1score: ..?</li>
@@ -96,13 +99,13 @@ function Reports() {
     return(
         <Card>
             <CardHeader>
-                <CardDescription>
+                <CardDescription className="text-base">
                     <p>You may click the new report button to file a new field report</p>
                 </CardDescription>
-                <CardDescription>
+                <CardDescription className="text-base">
                     <p>.. Explaination of the field report needed??</p>
                 </CardDescription>
-                <CardDescription>
+                <CardDescription className="text-base">
                     <p>You may search and filter different reports</p>
                 </CardDescription>
             </CardHeader>
@@ -114,16 +117,14 @@ function Patrol() {
     return(
         <Card>
             <CardHeader>
-                <CardDescription>
-                    <CardTitle>Route Parameters</CardTitle>
-                    <CardDescription>
-                        <ul>
-                            <li>Start location: Set the start location for the patrol route</li>
-                            <li>Patrol duration: Set how long you would like your patrol to roughly take in hours</li>
-                            <li>Priority: Select what options the algorithm should prioritize when generating the route. are these self explainitory??</li>
-                            <li>Suggested route: Give the stats of the generated route</li>
-                        </ul>
-                    </CardDescription>
+                <CardTitle className="text-lg">Route Parameters</CardTitle>
+                <CardDescription className="text-base">
+                    <ul>
+                        <li>Start location: Set the start location for the patrol route</li>
+                        <li>Patrol duration: Set how long you would like your patrol to roughly take in hours</li>
+                        <li>Priority: Select what options the algorithm should prioritize when generating the route. are these self explainitory??</li>
+                        <li>Suggested route: Give the stats of the generated route</li>
+                    </ul>
                 </CardDescription>
             </CardHeader>
         </Card>
@@ -134,13 +135,13 @@ function Profile() {
     return(
         <Card>
             <CardHeader>
-                <CardTitle>Profile details</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Profile details</CardTitle>
+                <CardDescription className="text-base">
                     <p>You may change your first and/or last name</p>
                 </CardDescription>
 
-                <CardTitle>Changing your password</CardTitle>
-                <CardDescription>
+                <CardTitle className="text-lg">Changing your password</CardTitle>
+                <CardDescription className="text-base">
                     <ul>
                         <li>You need to enter your current password</li>
                         <li>You must follow the onscreen suggestion to create your new password (must be 8 characters long)</li>
@@ -159,12 +160,12 @@ export default function HelpPage() {
         <div className="mx-auto max-w-[1120px] px-4 pt-8 pb-10 md:px-6">
             <Tabs defaultValue="faq">
                 <TabsList className="bg-(--color-color-surface-raised)">
-                    <TabsTrigger value="faq">FAQs</TabsTrigger>
-                    <TabsTrigger value="dashboard">Dashboard</TabsTrigger>
-                    <TabsTrigger value="map">Map</TabsTrigger>
-                    <TabsTrigger value="reports">Reports</TabsTrigger>
-                    <TabsTrigger value="patrol">Patrol Planner</TabsTrigger>
-                    <TabsTrigger value="profile">User Profile</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="faq">FAQ</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="dashboard">Dashboard</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="map">Map</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="reports">Reports</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="patrol">Patrol Planner</TabsTrigger>
+                    <TabsTrigger className="text-xl" value="profile">User Profile</TabsTrigger>
                 </TabsList>
 
                 <TabsContent value="faq">
