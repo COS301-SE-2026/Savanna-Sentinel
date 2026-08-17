b80979a5b44fb28bfd8fdc194547a6261c9f5e0c
Stephan-Kritzinger <u24581705@tuks.co.za>
2026-08-17 15:41:59 +0200

    Added the rest of the boundary processing logic

--- backend/app/services/risk_service.py
+++ backend/app/services/risk_service.py
@@ -1,7 +1,10 @@
 import io
+import math
 
 import geopandas
+import numpy
 from fastapi import HTTPException, status
+from shapely.geometry import box
 
 from app.repositories.risk_repository import load_grid_geometry
 from app.schemas.geo import GeoPolygon
@@ -55,4 +58,51 @@ def validate_boundaries(file: bytes):
     if parsed.crs is None:
         parsed.set_crs(espg=4326, inplace=True)
 
-    
+    center = parsed.geometry.union_all().centroid
+
+    # Determine UTM Zone
+    utm_zone = math.floor((center.x + 180) / 6) + 1
+    if center.y >= 0:
+        epsg_utm_zone = 32600 + utm_zone
+    else:
+        epsg_utm_zone = 32700 + utm_zone
+
+    parsed_utm = parsed.to_crs(epsg=epsg_utm_zone)
+
+    # Generate the grid to be used for the overlay later for intersection
+    cell_size = 1000
+    u_minx, u_miny, u_maxx, u_maxy = parsed_utm.total_bounds
+
+    grid_minx = numpy.floor(u_minx / cell_size) * cell_size
+    grid_miny = numpy.floor(u_miny / cell_size) * cell_size
+    grid_maxx = numpy.floor(u_maxx / cell_size) * cell_size
+    grid_maxy = numpy.floor(u_maxy / cell_size) * cell_size
+
+    x_coords = numpy.arrange(grid_minx, grid_maxx, cell_size)
+    y_coords = numpy.arrange(grid_miny, grid_maxy, cell_size)
+
+    cells = [
+        box(x, y, x + cell_size, y + cell_size)
+        for x in x_coords
+        for y in y_coords
+    ]
+
+    grid = geopandas.GeoDataFrame(geometry=cells, crs=utm_zone.crs)
+
+    # Generate the boundary
+    boundary_outline = parsed_utm.union_all()
+    full_blocks = grid[grid.intersects(boundary_outline)].copy()
+
+    # Attach metadata for maplibre
+    full_blocks["cell_id"] = [f"cell-{i}" for i in range(len(full_blocks))]
+
+    # Turn UTM back to WGS
+    full_blocks_wgs = full_blocks.to_crs(epsg=4326)
+
+    # Save to file
+    output_file = "reserve-grid.geojson"
+    full_blocks_wgs.to_file(output_file, driver="GeoJSON")
+
+    return {
+        "total_cells": len(full_blocks_wgs),
+    }
